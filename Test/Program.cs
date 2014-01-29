dfd9a169d4ff261a328cdf872d796da5198f1be7
Nero Sule <sulenero@gmail.com>
2014-01-29 20:57:32 +0000

    Update

--- Test/Program.cs
+++ Test/Program.cs
@@ -30,38 +30,46 @@ namespace Test
             var transformed = generated.TransformText();
             var provider = new CSharpCodeProvider();
             var compilerParams = new CompilerParameters();
-            compilerParams.GenerateInMemory = true;
-            compilerParams.OutputAssembly = "Automated";
+
+            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Migrations\\";
+
+            //compilerParams.GenerateInMemory = true;
+            compilerParams.GenerateInMemory = false;
+            compilerParams.OutputAssembly = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Context.dll";
             compilerParams.ReferencedAssemblies.Add("EntityFramework.dll");
             compilerParams.ReferencedAssemblies.Add("System.Data.dll");
             compilerParams.ReferencedAssemblies.Add("System.Data.Entity.dll");
             compilerParams.ReferencedAssemblies.Add("Core.dll");
             compilerParams.ReferencedAssemblies.Add("System.Core.dll");
             compilerParams.ReferencedAssemblies.Add("System.dll");
+            
+            //compilerParams.CompilerOptions = 
             var icc = provider.CompileAssemblyFromSource(compilerParams, transformed);
             var context =
                 (DbContext)
                     icc.CompiledAssembly.CreateInstance("EFMigrations.Context", false, BindingFlags.CreateInstance, null,
                         new object[] {"DefaultConnection"}, CultureInfo.CurrentCulture, null);
 
-            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Migrations\\";
-
+            
             var configuration = (DbMigrationsConfiguration)icc.CompiledAssembly.CreateInstance("EFMigrations.Configuration");
             
-            var scaffolder = new MigrationScaffolder(configuration);
-            var scaffold = scaffolder.Scaffold(DateTime.Now.ToString("hh_mm_ss"));
+            var scaffolder = new MigrationScaffolder(configuration) {Namespace = "EFMigrations"};
+            //var scaffold = scaffolder.Scaffold(DateTime.Now.ToString("hh_mm_ss"));
+            var scaffold = scaffolder.Scaffold("First");
             Directory.CreateDirectory(directory);
-            File.WriteAllText(directory + scaffold.MigrationId + ".designer.cs", scaffold.DesignerCode);
-            File.WriteAllText(directory + scaffold.MigrationId + ".cs", scaffold.UserCode);
-            using (var writer = new ResXResourceWriter(directory + scaffold.MigrationId + ".resources"))
-                foreach (var resource in scaffold.Resources)
-                    writer.AddResource(resource.Key, resource.Value);
+            //File.WriteAllText(directory + scaffold.MigrationId + ".designer.cs", scaffold.DesignerCode);
+            //File.WriteAllText(directory + scaffold.MigrationId + ".cs", scaffold.UserCode);
+            //using (var writer = new ResXResourceWriter(directory + "EFMigrations." + scaffold.MigrationId.Substring(scaffold.MigrationId.IndexOf("_", StringComparison.Ordinal)+1) + ".resources"))
+            //    foreach (var resource in scaffold.Resources)
+            //        writer.AddResource(resource.Key, resource.Value);
                 
-            
-
             var filesContents = Directory.GetFiles(directory).Where(x => x.EndsWith(".cs")).Select(File.ReadAllText).ToList();
-            var resources = Directory.GetFiles(directory).Where(x => x.EndsWith(".resources"));
-            compilerParams.OutputAssembly = "AutomatedMigrations";
+            var resources = Directory.GetFiles(directory).Where(x => x.EndsWith(".resx"));
+            foreach(var res in resources)
+
+            compilerParams.OutputAssembly = "AutomatedMigrations.dll";
+            //foreach (var resource in resources)
+                //compilerParams.CompilerOptions += string.Format("/resource:\"{0}\" ", resource);
             compilerParams.EmbeddedResources.AddRange(resources.ToArray());
             
             var assemblies = provider.CompileAssemblyFromSource(compilerParams, filesContents.ToArray());
@@ -69,8 +77,12 @@ namespace Test
             
             configuration.MigrationsAssembly = assemblies.CompiledAssembly;
             configuration.MigrationsNamespace = "EFMigrations";
+            //configuration.MigrationsDirectory = "Test\\bin\\Debug\\Migrations\\";*/
             var migrator = new DbMigrator(configuration);
-            migrator.Update();
+            var pending = migrator.GetPendingMigrations();
+            var resManager = new ResourceManager("EFMigrations.First", assemblies.CompiledAssembly);
+            var ress = resManager.GetResourceSet(CultureInfo.InvariantCulture, false, true);
+            migrator.Update("");
 
             var decorator = new MigratorScriptingDecorator(migrator);
             var codegenerator = new CSharpMigrationCodeGenerator();
@@ -80,11 +92,6 @@ namespace Test
             Console.Read();
             
         }
-    }
-
-    public class MigrationsConfiguration : DbMigrationsConfiguration<DbContext>
-    {
-        
     }
 }
 
