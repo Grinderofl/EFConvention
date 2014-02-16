7025972a402405d3faa8fc2db9636b10fc266fe3
Nero Sule <sulenero@gmail.com>
2014-02-16 21:24:19 +0000

    Fixed too many db calls.

--- WebTest/Global.asax.cs
+++ WebTest/Global.asax.cs
@@ -16,6 +16,7 @@ namespace WebTest
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            HibernatingRhinos.Profiler.Appender.EntityFramework.EntityFrameworkProfiler.Initialize();
         }
     }
 }
